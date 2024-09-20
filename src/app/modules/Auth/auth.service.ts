9f1284c0746666420cc40743d9312e0d3be8ad4d
goni715 <halesgoni715149123@gmail.com>
2024-09-20 11:10:03 +0600

    user-checking

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,7 +1,41 @@
+import httpStatus from "http-status";
+import AppError from "../../errors/AppError";
+import UserModel from "../user/user.model"
 import { TLoginUser } from "./auth.interface"
+import bcrypt from 'bcryptjs';
 
 
-const loginUserService = (payload: TLoginUser) => {
+const loginUserService = async (payload: TLoginUser) => {
+
+    const {id, password} = payload || {};
+
+    //check if the user is exist
+    const isUserExists = await UserModel.findOne({ id: id });
+    if(!isUserExists){
+        throw new AppError(httpStatus.NOT_FOUND, `This user is not found`);
+    }
+
+
+   
+     //check if the user is already deleted
+     const isDeleted = isUserExists?.isDeleted;
+     if(isDeleted){
+         throw new AppError(httpStatus.FORBIDDEN, `This user is already deleted`);
+     }
+
+
+      //check if the user is already deleted
+      const blockStatus = isUserExists?.status;
+      if(blockStatus === "blocked"){
+          throw new AppError(httpStatus.FORBIDDEN, `This user is blocked`);
+      }
+
+      //checking if the password is not correct
+      const isPasswordMatched = await bcrypt.compare(password, isUserExists?.password); //return true or false
+      if(!isPasswordMatched){
+          throw new AppError(httpStatus.FORBIDDEN, `Wrong Password`);
+      }
+
     return payload
 }
 
